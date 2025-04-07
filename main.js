029669eb7e6cbb364da5d79daec02afed4aa1588
Susã <141150681+noetzoldsusa@users.noreply.github.com>
2025-04-07 09:25:06 -0300

    yy

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2025-20-12T00:00:00");
 const tempoObjetivo2 = new Date("2025-14-12T00:00:00");
 const tempoObjetivo3 = new Date("2025-05-05T00:00:00");
-const tempoObjetivo4 = new Date("2025-25-11T00:00:00");
+const tempoObjetivo4 = new Date("2025-25-11T00:30:00");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
