d6c48f14c9f7d92be49c9153caf104d92ae96e0c
Susã <141150681+noetzoldsusa@users.noreply.github.com>
2025-04-07 09:26:02 -0300

    kkjjkjkjkjk

--- main.js
+++ main.js
@@ -15,10 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
 }
 
 const contadores = document.querySelectorAll(".contador");
-const tempoObjetivo1 = new Date("2025-20-12T00:00:00");
-const tempoObjetivo2 = new Date("2025-14-12T00:00:00");
-const tempoObjetivo3 = new Date("2025-05-05T00:30:00");
-const tempoObjetivo4 = new Date("2025-25-11T00:30:00");
+const tempoObjetivo1 = new Date("2025-20-12T00:00:01");
+const tempoObjetivo2 = new Date("2025-14-12T00:00:01");
+const tempoObjetivo3 = new Date("2025-05-05T00:00:01");
+const tempoObjetivo4 = new Date("2025-25-11T00:00:01");
 
 const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 
